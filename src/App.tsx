35cc5aeef7805f661521f650901b96d2f9b3dda5
Ilya Boyandin <ilyabo@gmail.com>
2021-06-21 16:38:11 +0200

    Update to latest SDK style

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React, {useEffect, useMemo, useState} from 'react';
 import './App.css';
 import {
-  Layer, createMap, getLayers,
-  setMapControlVisibility,
-  setTimelineConfig,
-  setLayerVisibility,
-  MapInstance
+  Layer, UnfoldedMap
 } from '@unfolded/map-sdk';
 
 function App() {
-  const [map, setMap] = useState<MapInstance>();
+  const [map, setMap] = useState<UnfoldedMap>();
   const [layers, setLayers] = useState<Layer[]>();
   const [selectedLayerId, setSelectedLayer] = useState<string>();
   const aiqLayerId = useMemo(() =>
@@ -20,33 +16,32 @@ function App() {
     if (layerId === selectedLayerId) return;
     setSelectedLayer(layerId);
     if (aiqLayerId != null) {
-      await setTimelineConfig(map, { idx: 0, isVisible: layerId === aiqLayerId });
+      await map.setTimelineConfig({ idx: 0, isVisible: layerId === aiqLayerId });
     }
     for (const {id} of layers) {
       const isVisible = id === layerId;
-      await setLayerVisibility(map, id, isVisible);
+      await map.setLayerVisibility(id, isVisible);
     }
   };
   useEffect(() => {
-    const mapInstance = createMap({
+    const _map = new UnfoldedMap({
       mapUUID: '84c26003-2d8c-4b61-b55d-964c1a043e35',
       appendToDocument: true,
       width: window.innerWidth,
       height: window.innerHeight,
       embed: true,
       onLoad: async function () {
-        const mapLayers = await getLayers(mapInstance);
+        const mapLayers = await _map.getLayers();
         setLayers(mapLayers);
         if (mapLayers?.length > 0) {
           await handleSelectLayer(mapLayers[0].id);
         }
-        await setMapControlVisibility(mapInstance, {panelId: 'mapLegend', isVisible: false});
+        await _map.setMapControlVisibility({panelId: 'mapLegend', isVisible: false});
       }
     });
-    setMap(mapInstance);
+    setMap(_map);
   }, []);
 
-  console.log(selectedLayerId);
   return (
     <>
       {layers &&
