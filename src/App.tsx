6119eed46271deedff5e66c6e3fe1cbf34699c7a
Ilya Boyandin <ilyabo@gmail.com>
2021-06-21 18:03:19 +0200

    Kontur app loading air quality data via SDK

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, {useEffect, useMemo, useState} from 'react';
 import './App.css';
-import {
-  Layer, UnfoldedMap
-} from '@unfolded/map-sdk';
+import {Layer, UnfoldedMap} from '@unfolded/map-sdk';
+import {csvParse} from 'd3-dsv';
+import {extent} from 'd3-array';
 
 function App() {
   const [map, setMap] = useState<UnfoldedMap>();
@@ -16,7 +16,7 @@ function App() {
     if (layerId === selectedLayerId) return;
     setSelectedLayer(layerId);
     if (aiqLayerId != null) {
-      await map.setTimelineConfig({ idx: 0, isVisible: layerId === aiqLayerId });
+      await map.setTimelineConfig({idx: 0, isVisible: layerId === aiqLayerId});
     }
     for (const {id} of layers) {
       const isVisible = id === layerId;
@@ -25,18 +25,20 @@ function App() {
   };
   useEffect(() => {
     const _map = new UnfoldedMap({
-      mapUUID: '84c26003-2d8c-4b61-b55d-964c1a043e35',
+      // mapUUID: '84c26003-2d8c-4b61-b55d-964c1a043e35',
+      mapUrl: 'http://localhost:8080/public/68a4fcdc-a625-4e33-b579-a98cbc4ab73b',
       appendToDocument: true,
       width: window.innerWidth,
       height: window.innerHeight,
       embed: true,
       onLoad: async function () {
+        _map.setMapControlVisibility({panelId: 'mapLegend', isVisible: false});
+        await addAirQualityLayer(_map);
         const mapLayers = await _map.getLayers();
         setLayers(mapLayers);
         if (mapLayers?.length > 0) {
-          await handleSelectLayer(mapLayers[0].id);
+          setSelectedLayer(mapLayers[0].id);
         }
-        await _map.setMapControlVisibility({panelId: 'mapLegend', isVisible: false});
       }
     });
     setMap(_map);
@@ -56,6 +58,64 @@ function App() {
       </ul>}
     </>
   );
+}
+
+
+async function addAirQualityLayer(map: UnfoldedMap) {
+  const response = await fetch('https://cdn.unfolded.ai/examples/aqi2.csv');
+  if (response.status === 200) {
+    const text = await response.text();
+    const rows = csvParse(text);
+    const timeExtent = extent(rows, d => new Date(d.last_seen!).getTime())
+    let datasetId = '94ed348e-8c98-4301-91ed-f6921dff09f1';
+    await map.addDataset(
+      {
+        uuid: datasetId,
+        label: 'Air Quality',
+        data: text
+      }, false
+    );
+    await map.addLayer({
+      id: 'aiq-layer',
+      type: 'point',
+      config: {
+        label: 'Air Quality (now)',
+        dataId: datasetId,
+        isVisible: false,
+        columns: {
+          lat: 'latitude',
+          lng: 'longitude'
+        },
+        colorScale: 'quantile',
+        colorField: {
+          name: 'aqi',
+          type: 'real'
+        },
+        visConfig: {
+          colorRange: {
+            colors:
+              ["#1a9850" ,
+                "#91cf60" ,
+                "#d9ef8b" ,
+                "#fee08b" ,
+                "#fc8d59" ,
+                "#d73027"
+              ]
+          }
+        }
+      }
+    });
+    await map.setFilter({
+      id: 'aiq-timeline-filter',
+      dataId: datasetId,
+      field: 'last_seen',
+      value: timeExtent
+    });
+    await map.setTimelineConfig({
+      idx: 0,
+      isVisible: false
+    });
+  }
 }
 
 export default App;
